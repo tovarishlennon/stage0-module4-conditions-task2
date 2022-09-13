cc049678abebad9f9ee5438e7d9ec53de32470ad
Azim Shamuratov <a.shamuratov@atmos.uz>
2022-09-13 11:05:03 +0500

    Task #2. Boolean Conditions

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        if(from1UpTo1000 < 500) {
+            System.out.println("Eagle");
+        } else if (from1UpTo1000 >= 500 && from1UpTo1000 < 1000) {
+            System.out.println("Tail");
+        }
     }
 }
